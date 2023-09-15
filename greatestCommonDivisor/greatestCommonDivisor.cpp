e97f54b98c33bfc11b5136b50bad9d63ce6138ba
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-09-15 21:02:33 +0300

    Add files via upload

---
+++ greatestCommonDivisor/greatestCommonDivisor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+
+	if (x >= y) {
+		for (int i = x; i > 0; i--) {
+			if (x % i == 0 && y % i == 0) {
+				cout << i;
+				break;
+			}
+		}
+	}
+	else {
+		for (int i = y; i > 0; i--) {
+			if (x % i == 0 && y % i == 0) {
+				cout << i;
+				break;
+			}
+		}
+
+	}
+}
+
