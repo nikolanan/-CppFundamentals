7e4a6cd2f75da4239aeb503cc8577eb9f203a7db
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-09-24 23:23:47 +0300

    Add files via upload

---
+++ factorialDivision/factorialDivision.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+long long factorial(long long number) {
+	if (number > 1) {
+		return number * factorial(number - 1);
+	}
+	else {
+		return 1;
+	}
+}
+int main(){
+	int a, b;
+	cin >> a >> b;
+	double firstResult = factorial(a);
+	double secondResult = factorial(b);
+	double finalResult = firstResult / secondResult;
+
+	cout.setf(ios::fixed);
+	cout.precision(2);
+
+	cout << finalResult;
+}
+
